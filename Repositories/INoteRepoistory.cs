e3f3f52d575fbbd42522085211ed3527f69c3b8e
Muskan Srivastava <srivastavamuskan77@gmail.com>
2020-12-03 14:58:35 +0530

    added database services to project

---
+++ Repositories/INoteRepoistory.cs
@@ -0,0 +1,18 @@
+﻿using NotelyRESTApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotelyRESTApi.Repositories
+{
+    public interface INoteRepoistory
+    {
+        Note FindById(long id);
+        public IEnumerable<Note> GetAllNotes();
+        public void SaveNote(Note n);
+        public void EditNote(Note n);
+        public void Delete(Note note);
+
+    }
+}
---
+++ Repositories/Implementation/NoteRepositoy.cs
@@ -0,0 +1,47 @@
+﻿using NotelyRESTApi.Database;
+using NotelyRESTApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotelyRESTApi.Repositories.Implementation
+{
+    public class NoteRepositoy : INoteRepoistory
+    {
+
+        private NotelyDbContext _context;
+
+        public NoteRepositoy(NotelyDbContext context)
+        {
+            _context = context;
+        }
+
+        public Note FindById(long id)
+        {
+            var note = _context.Notes.Find(id);
+            return note;
+        }
+        public IEnumerable<Note> GetAllNotes()
+        {
+            var note = _context.Notes;
+            return note;
+        }
+        public void SaveNote(Note n)
+        {
+            _context.Notes.Add(n);
+            _context.SaveChanges();
+        }
+        public void EditNote(Note n)
+        {
+            _context.Notes.Update(n);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Note note)
+        {
+            _context.Notes.Remove(note);
+            _context.SaveChanges();
+        }
+    }
+}
